79397d59bfcbf3600ce62dc09f2bc007fc75d410
feras.Oz <ferasosama99@gmail.com>
2026-05-01 14:12:57 +0300

    - Expense feature finished

--- lib/features/expenses/data/models/expense_model.g.dart
+++ lib/features/expenses/data/models/expense_model.g.dart
@@ -2,14 +2,18 @@
 
 part of 'expense_model.dart';
 
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
 ExpenseModel _$ExpenseModelFromJson(Map<String, dynamic> json) => ExpenseModel(
-      id: json['id'] as String,
-      title: json['title'] as String,
-      amount: (json['amount'] as num).toDouble(),
-      categoryId: json['categoryId'] as String,
-      date: DateTime.parse(json['date'] as String),
-      note: json['note'] as String?,
-    );
+  id: json['id'] as String,
+  title: json['title'] as String,
+  amount: (json['amount'] as num).toDouble(),
+  categoryId: json['categoryId'] as String,
+  date: DateTime.parse(json['date'] as String),
+  note: json['note'] as String?,
+);
 
 Map<String, dynamic> _$ExpenseModelToJson(ExpenseModel instance) =>
     <String, dynamic>{
